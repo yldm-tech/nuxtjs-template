c22ae8844e5c7d01b79e0e2fe241d4be51914d8b
Evan <suzukaze.hazuki2020@gmail.com>
2023-08-30 12:21:22 +0900

    add pinia and tailwind

---
+++ components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col justify-center mt-20">
+    <h1 class="bg-gray-800 text-white text-center">Click Counter</h1>
+    <p class="text-center">You have clicked the button {{ count }} times.</p>
+    <button
+      @click="counter"
+      class="self-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Click Me!
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from "~/stores";
+
+const { count, increment } = useCounterStore();
+const counter = () => {
+  console.log("before increment", count);
+  increment();
+  console.log("after increment", count);
+};
+</script>
